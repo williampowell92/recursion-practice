c7c0efc8abee6198543582a2514c560f8ad31a5b
William Powell <william.powell92@gmail.com>
2018-04-12 07:39:47 +0100

    Wrote bunny ears 2 method

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -11,3 +11,8 @@ end
 def fibonacci(n)
   n < 2 ? n : fibonacci(n - 2) + fibonacci(n - 1)
 end
+
+def bunny_ears_2(bunnies)
+  return 0 if bunnies == 0
+  bunnies.even? ? 3 + bunny_ears_2(bunnies - 1) : 2 + bunny_ears_2(bunnies - 1)
+end
